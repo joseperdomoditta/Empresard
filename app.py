23d1d7bb878f4a3813c892af1e4ebfeab27b18cb
joseperdomoditta <43288109+joseperdomoditta@users.noreply.github.com>
2026-04-23 01:03:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ scope = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
-creds = Credentials.from_service_account_file("credenciales.json", scopes=scope)
+creds = Credentials.from_service_account_file("/etc/secrets/credenciales.json", scopes=scope)
 client = gspread.authorize(creds)
 sheet = client.open(NOMBRE_HOJA).sheet1
 
